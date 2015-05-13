9ea78dc2ae947bd2b43e08374d13ad71cf8ca978
Eric So <me@eric.so>
2015-05-13 12:24:28 -0700

    Add templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,25 @@
+from flask import render_template
+
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, World!"
+  user = {'nickname': 'Eric'} # Fake user
+  posts = [ # Fake array of posts
+    {
+      'author': {'nickname': 'John'},
+      'body': 'Beautiful day in San Francisco!'
+    },{
+      'author': {'nickname': 'Susan'},
+      'body': 'The Avengers movie, soOooOOoOooo cool!'
+    }
+  ]
+
+  return render_template(
+    'index.html',
+    title='Home',
+    user=user,
+    posts=posts
+  )
