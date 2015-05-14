d50cd30c1545e198007ba66c3f34caa6bb18596b
Eric So <me@eric.so>
2015-05-14 06:12:14 -0700

    database management scripts

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config.from_object('config')
 
+db = SQLAlchemy(app)
+
 # Put views import after app creation to avoid circular import
-from app import views
+from app import views, models
