2550be537695ea0ac56375dcb7ca9c9e2c664da8
Eric So <me@eric.so>
2015-05-19 10:59:08 -0700

    Add user following functionality

--- app/models.py
+++ app/models.py
@@ -3,6 +3,14 @@ from hashlib import md5
 from app import db
 
 
+# Association table for linking users to followed users
+followers = db.Table(
+  'followers',
+  db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+  db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   nickname = db.Column(db.String(64), index=True, unique=True)
@@ -10,6 +18,14 @@ class User(db.Model):
   posts = db.relationship('Post', backref='author', lazy='dynamic')
   about_me = db.Column(db.String(140))
   last_seen = db.Column(db.DateTime)
+  followed = db.relationship(
+    'User',
+    secondary=followers,
+    primaryjoin=(followers.c.follower_id == id),
+    secondaryjoin=(followers.c.followed_id == id),
+    backref=db.backref('followers', lazy='dynamic'),
+    lazy='dynamic'
+  )
 
   # The following methods required by Flask-Login
   def is_authenticated(self):
@@ -27,10 +43,51 @@ class User(db.Model):
     except NameError:
       return str(self.id) # Python 3
 
+  # Use gravatar.com to get a user profile picture
   def avatar(self, size):
     return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (
       md5(self.email.encode('utf-8')).hexdigest(),
       size
+    )
+
+  # Following functionality
+  def follow(self, user):
+    """
+    Returns self if success, none if failure
+    Returned object has to be committed to db
+    """
+    if not self.is_following(user):
+      self.followed.append(user)
+      return self
+
+  def unfollow(self, user):
+    """
+    Returns self if success, none if failure
+    Returned object has to be committed to db
+    """
+    if self.is_following(user):
+      self.followed.remove(user)
+      return self
+
+  def is_following(self, user):
+    """
+    Returns True if self is following user, False otherwise
+    """
+    return self.followed.filter(
+      followers.c.followed_id == user.id
+    ).count() > 0
+
+  def followed_posts(self):
+    """
+    Returns the posts from users that this user follows
+    """
+    return Post.query.join(
+      followers,
+      (followers.c.followed_id == Post.user_id)
+    ).filter(
+      followers.c.follower_id == self.id
+    ).order_by(
+      Post.timestamp.desc()
     )
 
   @staticmethod
