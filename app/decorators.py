9e93fee9587e7d62b55f1dd102588a46ba2a7489
Eric So <me@eric.so>
2015-05-28 15:20:39 -0700

    Add Bootstrap static files and styling

--- app/decorators.py
+++ app/decorators.py
@@ -1,8 +1,13 @@
 from threading import Thread
+# from multiprocessing import Process
 
 
 def async(f):
   def wrapper(*args, **kwargs):
     thr = Thread(target=f, args=args, kwargs=kwargs)
     thr.start()
+
+    # p = Process(target=f, args=args, kwargs=kwargs)
+    # p.start()
+
   return wrapper
